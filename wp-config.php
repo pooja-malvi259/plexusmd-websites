d2bacf2ab4f289bf3a2afb7f61fe979dba9f8ca5
Pooja <malvi.pooja259@gmail.com>
2017-09-20 17:15:22 +0530

    Config File Changes

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'websites');
+define('DB_NAME', 'depc9gpdf6dnml');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'rjzhzlirtpcagn');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'a9fc130f37231ef57933be34156efb57fee4427dc910d65faa21e47cbbc389ed');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'ec2-54-221-212-208.compute-1.amazonaws.com:5432');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
